318bfa29c9243284dcd7714e05d73a2cd0d01ea8
do tien anh <anhdt.ute@gmail.com>
2021-05-18 21:52:52 +0700

    first app

---
+++ App.js
@@ -0,0 +1,21 @@
+/* eslint-disable react-native/no-inline-styles */
+import React, {Component} from 'react';
+import {SafeAreaView} from 'react-native';
+import Main from './src/screens/Main';
+import Box from './src/components/Box';
+import Form from './src/components/Form';
+import Filter from './src/components/Filter';
+import Word from './src/components/Word';
+
+export default class App extends Component {
+  render() {
+    return (
+      <SafeAreaView style={{flex: 1}}>
+        {/* <Filter /> */}
+        {/* <Main /> */}
+        <Word />
+        {/* <Form/> */}
+      </SafeAreaView>
+    );
+  }
+}
---
+++ src/components/Filter.js
@@ -0,0 +1,42 @@
+/* eslint-disable no-alert */
+/* eslint-disable no-shadow */
+/* eslint-disable no-unused-vars */
+import React, {Component} from 'react';
+import {StyleSheet, View} from 'react-native';
+import {RNPickerSelect, RNCPicker} from 'react-native-picker-select';
+
+export default class Filter extends Component {
+  render() {
+    let value = null;
+    return (
+      <View style={styles.containerPickerStyle}>
+        <RNPickerSelect
+          onValueChange={(value) => {
+            this.value = value;
+          }}
+          onDonePress={() => {
+            alert(this.value);
+          }}
+          items={[
+            {label: 'Show All', value: 'Show_All'},
+            {label: 'Show Forgot', value: 'Show_Forgot'},
+            {label: 'Show Memorized', value: 'Show_Memorized'},
+          ]}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  containerPickerStyle: {
+    borderWidth: 1,
+    borderRadius: 1,
+    borderColor: 'black',
+    padding: 20,
+    marginBottom: 5,
+  },
+  pickerStyle: {
+    padding: 50,
+  },
+});
